6d12371156a8d5dc1225ef6c1c9de464a3f24b47
yoshinator <yoanante@gmail.com>
2018-09-21 12:52:20 -0400

    Yielding properly

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+  else
+    array
+  end
+  array
 end
